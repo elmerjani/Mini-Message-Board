ff7cf3d57079b209f3dc5c5dc808d9d980dba005
elmerjani <mohamedelmerjani3@gmail.com>
2024-09-09 16:15:39 +0100

    Set up the root route

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+  res.render('index');
+});
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
